f0f58cfdac15c052cd5c3a6b7f9098c6f0fd167b
wellingtonxafundifornio <wellingtonxafundifornio@gmail.com>
2018-06-18 18:00:40 -0300

    first commit

---
+++ HelloXamarin/HelloXamarin.Android/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Plugin.Media;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+
+namespace HelloXamarin.Droid
+{
+    [Activity(Label = "HelloXamarin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override async void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            await CrossMedia.Current.Initialize();
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
---
+++ HelloXamarin/HelloXamarin/MainPage.xaml.cs
@@ -0,0 +1,149 @@
+﻿using Plugin.Geolocator;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloXamarin
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void TirarFoto(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsTakePhotoSupported || !CrossMedia.Current.IsCameraAvailable)
+            {
+                await DisplayAlert("Ops", "Nenhuma câmera detectada.", "OK");
+
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(
+                new StoreCameraMediaOptions
+                {
+                    SaveToAlbum = true,
+                    Directory = "Demo"
+                });
+
+            if (file == null)
+                return;
+
+            MinhaImagem.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+
+            });
+        }
+
+        private async void EscolherFoto(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Ops", "Galeria de fotos não suportada.", "OK");
+
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync();
+
+            if (file == null)
+                return;
+
+            MinhaImagem.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+
+            });
+        }
+
+        private async void GravarVideo(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsTakeVideoSupported || !CrossMedia.Current.IsCameraAvailable)
+            {
+                await DisplayAlert("Ops", "Nenhuma câmera detectada.", "OK");
+
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakeVideoAsync(
+                new StoreVideoOptions
+                {
+                    SaveToAlbum = true,
+                    Directory = "Demo",
+                    Quality = VideoQuality.Medium
+                });
+
+            if (file == null)
+                return;
+
+            MinhaImagem.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+
+            });
+        }
+
+        private async void EscolherVideo(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickVideoSupported)
+            {
+                await DisplayAlert("Ops", "Galeria de videos não suportada.", "OK");
+
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickVideoAsync();
+
+            if (file == null)
+                return;
+
+            MinhaImagem.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+
+            });
+        }
+
+        private async void GetPosition(object sender, EventArgs e)
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 50;
+
+            try
+            {
+                var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
+                LongitudeLabel.Text = string.Format("{0:0.0000000}", position.Longitude);
+                LatitudeLabel.Text = string.Format("{0:0.0000000}", position.Latitude);
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+            }
+            
+        }
+    }
+}
